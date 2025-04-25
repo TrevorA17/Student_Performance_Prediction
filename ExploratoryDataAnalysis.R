5be09e8bd8891bb2dc8158ad866cd1727d826966
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2025-04-25 16:12:34 +0300

    Update ExploratoryDataAnalysis.R

---
+++ ExploratoryDataAnalysis.R
@@ -0,0 +1,43 @@
+# Load dataset
+StudentData <- read.csv("data/student_performance.csv", colClasses = c(
+  Gender = "factor",
+  AttendanceRate = "numeric",
+  StudyHoursPerWeek = "numeric",
+  PreviousGrade = "numeric",
+  ExtracurricularActivities = "numeric",
+  ParentalSupport = "factor",
+  FinalGrade = "numeric"
+))
+
+# Preview structure and data
+str(StudentData)
+head(StudentData)
+View(StudentData)
+
+# Measures of Frequency
+# Frequency of Gender
+table(StudentData$Gender)
+
+# Frequency of Parental Support levels
+table(StudentData$ParentalSupport)
+
+# Measures of Central Tendency
+# Central Tendency: Mean & Median
+mean(StudentData$AttendanceRate)
+median(StudentData$AttendanceRate)
+
+mean(StudentData$StudyHoursPerWeek)
+median(StudentData$StudyHoursPerWeek)
+
+mean(StudentData$FinalGrade)
+median(StudentData$FinalGrade)
+
+# Mode function (for categorical/factor or numeric variables)
+get_mode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+get_mode(StudentData$ParentalSupport)
+get_mode(StudentData$Gender)
+
